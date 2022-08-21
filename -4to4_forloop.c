1d9b4dc38415a2aa8fd563e35ddc1e7e4fffbe33
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 15:08:50 +0530

    Create -4to4_forloop.c

---
+++ -4to4_forloop.c
@@ -0,0 +1,18 @@
+//Author : Yogita shidore
+//Date : 21/08/2022
+//print number -4 to 4             
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   int i;
+   for (i = -4; i <= 4; i = i+1)
+   {
+    
+     printf("%d",i);
+ }
+   return(0); //returning value - successful
+}
