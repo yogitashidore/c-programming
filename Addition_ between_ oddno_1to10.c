3ba93e1aac865363b62b498ca8ee453a589b8cec
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 15:03:17 +0530

    Create Addition_ between_ oddno_1to10.c

---
+++ Addition_ between_ oddno_1to10.c
@@ -0,0 +1,19 @@
+//Author : Yogita shidore
+//Date : 21/08/2022
+//print addition of all odd numbers between 1 to 10 for loop              
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   int i,sum;
+   for (i = 1; i <= 10; i = i+2)
+   {
+    
+      sum=sum+i;
+ }
+ printf("\n addition of all odd numbers between 1to 10 +%d,sum");
+   return(0); //returning value - successful
+}
