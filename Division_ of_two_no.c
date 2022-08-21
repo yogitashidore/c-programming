3e33eb83e42844316d33eb297cca8154f538c704
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:09:09 +0530

    Create Division_ of_two_no.c

---
+++ Division_ of_two_no.c
@@ -0,0 +1,20 @@
+//program name : print msg on terminal
+//Assignment : 4
+//Date : 15/08/2022
+//Write a program for division of two numbers
+ 
+//input:
+
+//include std input output file
+#include<stdio.h>
+int main()
+{ 
+   int a, b, div;
+   printf("\nEnter the value of a:");  //print msg on terminal
+   scanf("%d", &a);
+   printf("\nEnter the value of b:");
+   scanf(%d", &b);
+   div=a \ b;
+   printf("\nDivision of a & b is: %d", div);
+   return(0);  //returning responce - successful
+}
