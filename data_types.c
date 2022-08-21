6308ba0bd3a06950da4ee91298553fc3c26ab214
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:31:08 +0530

    Create data_types.c

---
+++ data_types.c
@@ -0,0 +1,19 @@
+//Date : 16/08/2022
+//Author : Yogita Shidore
+
+//input : 
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+int main()
+{
+   //local variable declaration
+   float f = 12.5f;
+   double d = 12.45;
+   
+   printf("\nvalue of f = %f", f);  //print msg on terminal
+   printf("\nvalue of d = %1f", d);
+   
+   return(0);  //returning value - successful
+}   
