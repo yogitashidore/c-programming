1eb033a29d1ec5f7fe0a63ac011f7a45022b6948
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:24:03 +0530

    Create double_datatypes.c

---
+++ double_datatypes.c
@@ -0,0 +1,14 @@
+//Author - Yogita shidore
+//Date - 16/08/2022
+//Write a c-program print size of double datatypes
+
+//input
+
+//include std input output file
+#include<stdio.h>
+int main()
+{ 
+   double d;
+   printf("size of an double is %1f \n\n",size of(d));  //print msg on terminal
+   return(0); //return type value 
+}
