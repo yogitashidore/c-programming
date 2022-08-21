ee1e5bafa3c407f0b503a08764d8dadb25bc8f77
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 15:24:23 +0530

    Create Addition_oddno_bet_1to100.c

---
+++ Addition_oddno_bet_1to100.c
@@ -0,0 +1,20 @@
+
+//Author : Yogita shidore
+//Date : 21/08/2022
+//print addition of all odd numbers between 1 to 100          
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   int i,sum;
+   for (i = 1; i <= 100; i = i+1)
+   {
+    
+     sum=sum+i;
+ }
+    printf("\naddition of all odd numbers between 1 to 100 = %d",sum);
+   return(0); //returning value - successful
+}
