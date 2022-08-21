c54498cf41be34a4668c6d647665b0187ef6d9ee
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:51:37 +0530

    Create Float-datatype.c

---
+++ Float-datatype.c
@@ -0,0 +1,16 @@
+//Author : Yogita shidore
+//Date : 16/08/2022
+//Write a c-program print size of float datatypes
+
+//input :
+//include std input output file
+
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+float f;
+printf("\n\nsize of float =%d",size of(f0);  //print msg on terminal
+return(0); //returning value -successful
+}
