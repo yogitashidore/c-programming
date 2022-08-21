77dfca591a33e7151ba0fde54838a38a25edef52
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 14:12:56 +0530

    Create Table3_forloop.c

---
+++ Table3_forloop.c
@@ -0,0 +1,19 @@
+//Author : Yogita shidore
+//Date : 18/08/2022
+//print table of 3 using for loop
+
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   //local variable declaration
+   int i;
+   for (i = 3; i <= 30; i = i + 3)
+   {
+      printf("\n%d", i);
+      
+   }
+   return(0); //returning value - successful
+}
