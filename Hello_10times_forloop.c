ec675467aca8bfa62693f9c06dc3d9044ba452c3
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 13:37:41 +0530

    Create Hello_10times_forloop.c

---
+++ Hello_10times_forloop.c
@@ -0,0 +1,18 @@
+//Author : Yogita shidore
+//Date : 17/08/2022
+//print Hello 10 times using for loop
+
+//input : 
+
+//include std input output file
+#include<stdio.h>
+int main()
+{
+  inti;
+  for(i=1; i<=10; i=i+1)
+  {
+    printf("\nHello");
+
+  }
+  return(0);  //returning value - successful 
+}
