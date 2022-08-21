dd9016f4c34d769d525328abf591e4dd0f7d3c40
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:48:20 +0530

    Create Character_datatype.c

---
+++ Character_datatype.c
@@ -0,0 +1,14 @@
+//Author : Yogita shidore
+//Date : 16/08/2022
+//Write a program print size of character datatypes
+
+//input
+//include std input output file
+
+#include<stdio.h>
+int main()
+{
+   char c;
+   printf("\n\nsize of char =%zu\n\n",size of(c));  //print msg on terminal
+   return(0);  //returning value - successful
+}
