421316750a41948e4882c6518fa30da268a8ae04
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 12:24:04 +0530

    Create Escap.c

---
+++ Escap.c
@@ -0,0 +1,16 @@
+//Author : Yogita shidore
+//Date : 16/08/2022
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point output file
+int main()
+{
+  printf("\n\nH\tello");  //print msg on terminal
+  printf("\n\nHe\tllo");
+  printf("\n\nHel\tlo");
+  printf("\n\nHell\to\n\n");
+    return(0);  //returning response - successful
+ }
