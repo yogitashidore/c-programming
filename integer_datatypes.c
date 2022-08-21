eef8a13d04391d7b9debeec240b35993a035cc4d
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:41:02 +0530

    Create integer_datatypes.c

---
+++ integer_datatypes.c
@@ -0,0 +1,15 @@
+//Author : Yogita shidore
+//Date : 16/08/2022
+//Write a c- program print size of integer datatypes
+
+//input :
+
+//include std input output file
+
+#include<stdio.h>
+int main()
+{
+   int iNo; //declare of an integer
+   printf("size of int is %d\n", size of(iNo));  //print msg on terminal
+   return(0); //return type value - successful
+}
