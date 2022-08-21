3a3ad78b1948061c4c93eb5c082372f543e409b3
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:44:52 +0530

    Create unsigned_value.c

---
+++ unsigned_value.c
@@ -0,0 +1,18 @@
+//Date : 16/08/2022
+//Author : Yogita Shidore
+
+//input :
+
+//include std input output file
+
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+   //local variable declarations
+  unsigned int num = 65000;
+  printf("unsigned int value = %u", num);  //print msg on terminal
+   
+   return(0); //returning response - successful
+}
