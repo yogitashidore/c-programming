b58eec20d68fb97c294498bd6c7aaaa171f95e37
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 14:31:57 +0530

    Create Helloword_11times_forloop.c

---
+++ Helloword_11times_forloop.c
@@ -0,0 +1,19 @@
+//Author : Yogita shidore
+//Date : 18/08/2022
+//print Helloword program 11 times               
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   
+   int i;
+   for (i = 0; i <= 11; i = i++)
+   {
+      printf("Helloword program, this is my first code in c");
+      
+   }
+   return(0); //returning value - successful
+}
