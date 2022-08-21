605c3290fedd9b95ee9011e8de45343e4bba08d2
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 10:49:42 +0530

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,18 @@
+//program name : print msg on terminal
+//Assignment : 1
+//Date :15-Aug-2022
+//Author : Yogita  Shidore
+
+//input
+
+
+//include sdt input output file
+#include<stdio.h>
+
+//entry point function
+int main() 
+{
+printf("Hello word"); // print mesg on treminal
+
+return 0:returning response - successful
+}
