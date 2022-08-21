959fe7d2d1ed37945055aecd14380083bef2a95f
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 14:23:21 +0530

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,22 @@
+//Author : Yogita shidore
+//Date : 18/08/2022
+//print 5
+        4
+        3
+        2
+        1
+        0
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   //local variable declaration
+   int i;
+   for (i = 5; i > -1; i = i - 1)
+   {
+      printf("\n%d", i);  //print msg on terminal
+      
+   }
+   return(0); //returning value - successful
