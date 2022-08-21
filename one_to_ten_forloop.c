ea458414b80d2b34cbf9b23318cd1af00d39cc72
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 14:43:30 +0530

    Create one_to_ten_forloop.c

---
+++ one_to_ten_forloop.c
@@ -0,0 +1,17 @@
+//Author : Yogita shidore
+//Date : 18/08/2022
+//print 1to10 for loop              
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   
+   for (i = 1; i <= 10; i = i++)
+   {
+      printf("\n%d",i)
+ }
+   return(0); //returning value - successful
+}
