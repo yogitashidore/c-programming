39914f371227c4350ab3f4a09cb960a49894cee5
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 11:06:22 +0530

    Create Multiplication_ of _two_ no.c

---
+++ Multiplication_ of _two_ no.c
@@ -0,0 +1,20 @@
+//program name : print msg on terminal
+//Assignment : 4
+//15-08-2022
+//Write a program for multiplication of two numbers.
+
+//input :
+
+//include sdt input output file
+#include<stdio.h>
+int main()
+{ 
+   int a, b, mul;
+   printf("\nEnter the value of a:");  //print msg on terminal
+   scanf("%d", &a);
+   printf("\nEnter the value of b:");
+   scanf("%d", &b);
+   mul=a*b
+   printf("\nMultiplication of a & b is : %d", mul);
+   return(0);  //returning responce - successful
+}
