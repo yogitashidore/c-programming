92ff8985da04865454033565d5b9f748c4921ff9
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-21 14:09:31 +0530

    Create Evenno-for_ loop.c

---
+++ Evenno-for_ loop.c
@@ -0,0 +1,20 @@
+
+//Author : Yogita shidore
+//Date : 18/08/2022
+//print even number 2to20 using for loop
+
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   //local variable declaration
+   int i;
+   for (i = 2; i <= 20; i = i + 2)
+   {
+      printf("\n%d", i);
+      
+   }
+   return(0); //returning value - successful
+}
