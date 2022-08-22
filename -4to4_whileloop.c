35f91674db2548702bf43bf4f49ae512c8984692
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 14:57:23 +0530

    Create -4to4_whileloop.c

---
+++ -4to4_whileloop.c
@@ -0,0 +1,19 @@
+//Author : Yogita shidore
+//Date : 21/08/2022
+//print number -4 to 4 in while loop           
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+   int i = -4;
+   while(i <= 4;)
+   {
+    
+     printf("%d",i);
+     i = i+1;
+ }
+   return(0); //returning value - successful
+}
