71de53b2a24a9f9cd33ccfa8cf50b5e3cec8ef75
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 15:26:21 +0530

    Create whileloop.c

---
+++ whileloop.c
@@ -0,0 +1,20 @@
+//Athor: Yogita shidore
+//Date : 22/08/2022
+//print 4 star in while loop
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int i=0;
+  while(i<4)
+  {
+     printf("\n****,i);
+     i++;
+  }
+   returning(0);
+}
