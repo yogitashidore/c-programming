cc1f328b42ce758378530c5a5bcaf6617b9c3a08
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 14:33:41 +0530

    Create table_17_forloop.c

---
+++ table_17_forloop.c
@@ -0,0 +1,20 @@
+//Athor : Yogita shidore
+//Date : 22/08/2022
+//print table of 17
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int i;
+  for(i=17; i<=170; i=i+17)
+  {
+    printf("\n%d",i);
+}
+
+   return(0); //returning value -successful
+}
