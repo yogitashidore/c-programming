8d86bc85d3586ca71e0a334f805d760594d6fd15
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 15:14:58 +0530

    Create Addition_evenno_whileloop.c

---
+++ Addition_evenno_whileloop.c
@@ -0,0 +1,23 @@
+//Athor : Yogita shidore
+//Date : 22/08/2022
+//Do addition of all even number 1to10 while loop
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int i=2;
+  int sum = 0;
+  while(i<=10)
+  {
+     printf("%d",i);
+     i=i+2
+     sum=sum+1;
+  }
+  printf("addition of all even number from 1 to 10is %d",sum);
+   return(0); //returning value -successful
+}
