dd5a47808afbf9631ba970a448b5ad37d8db7d6a
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 15:20:38 +0530

    Create evenn0_1to10_whileloop.c

---
+++ evenn0_1to10_whileloop.c
@@ -0,0 +1,20 @@
+//Athor: Yogita shidore
+//Date : 22/08/2022
+//print even number 1to10 while loop
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int i=2;
+  while(i<=10)
+  {
+     printf("%d",i);
+     i=i+2;
+  }
+   returning(0);
+}
