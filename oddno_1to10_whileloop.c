31f487a3c44461babcf605ad7daf9e4270dee30d
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 15:07:29 +0530

    Create oddno_1to10_whileloop.c

---
+++ oddno_1to10_whileloop.c
@@ -0,0 +1,20 @@
+//Athor : Yogita shidore
+//Date : 22/08/2022
+//print 1 to 10 number in while loop
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int i=1;
+  while(i<=10)
+  {
+     printf("%d",i);
+     i=i+2;
+  }
+   return(0); //returning value -successful
+}
