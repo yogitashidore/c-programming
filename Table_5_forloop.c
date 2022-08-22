8d23ec5c0385698e3209d75a8654286c9bc2b4a3
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 14:31:44 +0530

    Create Table_5_forloop.c

---
+++ Table_5_forloop.c
@@ -0,0 +1,20 @@
+//Athor : Yogita shidore
+//Date : 22/08/2022
+//print table of 5
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int i;
+  for(i=5; i<=50; i=i+5)
+  {
+    printf("\n%d",i);
+}
+
+   return(0); //returning value -successful
+}
