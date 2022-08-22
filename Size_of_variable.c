96c499bf65f0aa1940e4712d74fee158fc3182ac
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-22 14:44:18 +0530

    Create Size_of_variable.c

---
+++ Size_of_variable.c
@@ -0,0 +1,23 @@
+//Athor : Yogita shidore
+//Date : 22/08/2022
+//to find the size of variables
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+  int type;
+  float type;
+  double type;
+  char type;
+  //evaluate the size of variable
+  printf("size of int :%zu bytes\n",size of(int type));
+  printf("size of float :%zu bytes\n",size of(float type));
+  printf("size of doble :%zu bytes\n",size of(double type)); 
+ printf("size of char :%zu bytes\n",size of(char type));
+   return(0); //returning value -successful
+}
