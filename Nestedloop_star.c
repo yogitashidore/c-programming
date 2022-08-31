dffe7332bb91aad732091656f0481329cb65dde6
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-31 21:56:40 +0530

    Create Nestedloop_star.c

---
+++ Nestedloop_star.c
@@ -0,0 +1,27 @@
+//Author : Yogita shidore
+//Date : 31/08/2022
+//program for printing below pattern Nested loop
+   ****
+   ****
+   ****
+   ****
+//input
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+int main()
+{
+   int i;
+   int j;
+   for(j=0; j<4; j++)
+   {
+   for(i=0; i<4; i++)
+   {
+   printf("*");
+   }
+   printf("\n");
+   }
+   printf("\n");
+   return(0);
+}
