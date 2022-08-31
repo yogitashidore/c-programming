da6b92b6427f8b7889e759ae4f278e62ea20d4bc
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-08-31 21:44:53 +0530

    Create Nestedloop.c

---
+++ Nestedloop.c
@@ -0,0 +1,17 @@
+//Author : Yogita shidore
+//Date : 31/08/2022
+//Nested loop
+
+//input
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+int main()
+{
+printf("*\n");  //print msg on terminal
+return(0); //returning response
+}
+
+
+//output : *
