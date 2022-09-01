67c50f4c9113892a4dd6c4fd61fe7cd05a18ace1
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-09-01 18:10:34 +0530

    Create Nestedloop-11.c

---
+++ Nestedloop-11.c
@@ -0,0 +1,31 @@
+//Date :1/09/2022
+//Write a program for nested loop
+  print 
+        * * * * * *
+		*         *
+		*         *
+		*         *
+		*         *
+		* * * * * *
+//input :
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+	 int main()
+ {
+   int i;
+   int j;
+   for(j=1; j<=6; j++ )
+   {
+     for(i==1||j==1||i==6||j==5)
+	 {
+	   printf("*");
+     }
+	 else
+	 {
+	   printf("  ");
+	 }
+   }
+   printf("\n");
+   }
