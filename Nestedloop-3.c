91dff46c327b9185d72c0bdd97d97ef2ff3c5f2c
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-09-01 17:46:12 +0530

    Create Nestedloop-3.c

---
+++ Nestedloop-3.c
@@ -0,0 +1,19 @@
+//Author : Yogita shidore
+//Date :1/09/2022
+//Write a program for nested loop
+  print 1 2 3 4 5
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+	 int main()
+ {
+    int i;
+	for(i=1; i<=5; i++)
+	{
+	   printf("%d", i);  //print msg on terminal
+	}
+	return(0);  //returning value
+}
