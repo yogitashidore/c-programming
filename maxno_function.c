40094d9ab5ac02bccfad105927bcf3ff7b8be3d3
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-09-18 17:14:52 +0530

    Update maxno_function.c

--- maxno_function.c
+++ maxno_function.c
@@ -2,27 +2,32 @@
 //Date: 12/9/2022
 //Yogita shidore
 
-#include <stdio.h>
-int max(int no1, int no2);
-int main() {
-    int no1, no2, maximum;
-    printf("enter any two numbers: ");
-    scanf("%d%d", &no1, &no2);
-    
-    maximum = max(no1,no2);
-    
-   printf("\n maximum = %d\n", maximum);
-   if(no1>no2)
-   {
-       printf("n01 is greater than no2");
-   }
-   else
-   {
-       printf("no2 is greater than no1");
-   }
-    return 0;
-}
-   int max(int no1, int no2)
+#include<stdio.h>
+int main()
 {
-    return(no1>no2); 
+    //local variable declaration
+    int no1 = 50;
+    int no2 = 30;
+    int maximum;
+    //function declaration
+    int maximum(int no1, int no2);
+    //code
+    maximum = maximum(no1, no2);
+    printf("\n%d", maximum");
+    
+    return(0);
 }
+int maximum(int no1, int no2)
+{
+   int maximum;
+    if(no1>no2)
+    {
+       printf("no1 is greater than no2");  
+    }
+    else
+    {
+        printf("no2 is greater than no1");
+    }
+    return(maximum);
+}
+    
