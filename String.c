bf31a478608dc72a6c60b4768d9daaa67c810cfa
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-09-18 17:37:23 +0530

    Create String.c

---
+++ String.c
@@ -0,0 +1,18 @@
+//write a program to find a length
+//Date : 18/9/2022
+//Author : Yogita shidore
+
+#include<stdio.h>
+int main()
+{
+   char[] = "Yogita shidore, sgfdsah gfdh";
+   int i;
+   for(i = 0; s[i] !='\0'; i++);
+   {
+      //no code
+   }
+   {
+     printf("length of the string : %d", i);
+   }
+   return(0);
+}
