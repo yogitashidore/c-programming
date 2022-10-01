2741f650424c8031c8d19e20bd9116bd67e94551
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 12:00:10 +0530

    Create Array_7.c

---
+++ Array_7.c
@@ -0,0 +1,21 @@
+//Author : Yogita shidore
+//Date : 8/09/2022
+//Write a program for array.
+ 
+//input :
+
+//include std input output file
+#include<stdio.h>
+#include<math.h>
+	 int main()
+ { 
+    printf("\nargc = %d", argc);
+    printf("\n%d" a to i(argv[i]));
+    return(0);
+ }
+ 
+ 
+ 
+ //output
+  argc = 2
+    12
