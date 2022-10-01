117278e8a9ded101c5374651c0b2430cbe6c2476
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 11:21:59 +0530

    Create Array_3.c

---
+++ Array_3.c
@@ -0,0 +1,18 @@
+//Author : Yogita shidore
+//Date :7/09/2022
+//Write a program for Array
+ 
+//input :
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+	 int main()
+ {
+  int arr[5] = {10,20,30,40,50};
+  for(int i = 0; i<5; i++)
+  {
+  printf("\narr[%d] = %d", i, arr[i]);
+  }
+  return(0);
+}
