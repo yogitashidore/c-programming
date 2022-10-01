57b3fc34fc40d12fc6a25222f180e009ce389c1f
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 10:57:57 +0530

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,22 @@
+//Write a c program using array
+
+# include<stdio.h>
+int main()
+{
+int arr[5]
+//write
+arr[0] = 10;
+arr[1] = 20;
+arr[2] = 30;
+arr[3] = 40;
+arr[4] = 50;
+
+//Read
+printf("\nArr[0] = %d", arr[0]);
+printf("\nArr[1] = %d", arr[1]);
+printf("\nArr[2] = %d", arr[2]);
+printf("\nArr[3] = %d", arr[3]);
+printf("\nArr[4] = %d", arr[4]);
+
+return(0);
+}
