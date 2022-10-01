a01ce29d9cb07fb535895837f4ec7a587ee15697
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 11:40:49 +0530

    Create Array_6.c

---
+++ Array_6.c
@@ -0,0 +1,18 @@
+//Author : Yogita shidore
+//Date :8/09/2022
+//Write a program for array.
+ * * * * *
+//input :
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+	 int main()
+ {
+    char arr[5] = {'Y', 'O','G' 'I','T', 'A'}
+    for(int i =0; i <= 6; i++)
+    {
+      printf("%c", arr[i]);
+    }
+    return(0);
+  }
