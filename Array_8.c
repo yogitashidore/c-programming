816d169e6bb80eec8ec62318d7a57feb92ad35f7
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 12:10:29 +0530

    Create Array_8.c

---
+++ Array_8.c
@@ -0,0 +1,16 @@
+//Author : Yogita shidore
+//Date :9/09/2022
+//Write a program for array.
+ 
+//input :
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+	 int main(int argc, char *argv[])
+{
+  //code
+  printf("sum = %d", (a to i(argv[i]) + a to i (argv[2])
+   return(0);
+}
+  
