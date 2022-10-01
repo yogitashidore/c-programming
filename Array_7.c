29e23098ff18f9f324535d45169092c24f64bcb8
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 12:04:46 +0530

    Update Array_7.c

--- Array_7.c
+++ Array_7.c
@@ -7,7 +7,7 @@
 //include std input output file
 #include<stdio.h>
 #include<math.h>
-	 int main()
+	 int main(int argc, char *argv[])
  { 
     printf("\nargc = %d", argc);
     printf("\n%d" a to i(argv[i]));
