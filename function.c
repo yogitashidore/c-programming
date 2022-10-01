9d5214b77f3d91475e428e9e17564babade4e8dd
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 13:41:55 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,32 @@
+//Write a program for function.
+//Date : 10/9/2022
+//Author : Yogita shidore
+
+{
+   //local variable declaration
+     int num = 10;
+   //function declaration
+    void fun();
+    
+   //code
+   printf("\nvalue of num = %d", num);
+   printf("\nvalue of num = %d", num);
+   
+   //function call
+   fun();
+   return(0);
+}
+void fun()
+{
+  //local variable
+  int num = 10;
+  //code
+  printf("\nvalue of num = %d", num);
+}
+
+
+//output
+10
+10
+10
+  
