642eec1b588d469230514f1ea0d877f3a8361bb4
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 14:05:28 +0530

    Create function-2.c

---
+++ function-2.c
@@ -0,0 +1,25 @@
+//Wriite a program for function
+//Date :  10/9/2022
+//Author : Yogita shidore
+
+
+#include<stdio.h>
+int main()
+{
+   //function declaration
+   void fun();
+   //function call
+   fun();
+   return(0);
+}
+void fun()
+{
+  //local variable declaration
+  int count = 0;
+  //code
+  count = count + 1;
+  printf("\nvalue of num = %d", count);
+}
+
+
+//output = 1
