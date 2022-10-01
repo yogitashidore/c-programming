985ba90ded3009ae01398daa2b9a4506f01c13e3
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 11:08:51 +0530

    Create Array_2.c

---
+++ Array_2.c
@@ -0,0 +1,26 @@
+//Author : Yogita shidore
+//Date :5/09/2022
+//Write a program on array
+ 
+//input :
+
+//include std input output file
+#include<stdio.h>
+//entry point function
+	 int main()
+ {
+  int arr[5];
+  int i;
+  //write
+  for(i=0; i<5; i++)
+  {
+  arr[i] = [i+1] = 10
+  }
+  //Read
+  for(i+0; i<5; i++)
+  {
+   printf("\nArr[%d] = %d", i, arr[i]);
+  }
+  return(0);
+}
+   
