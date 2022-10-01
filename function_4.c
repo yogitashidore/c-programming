8efa30a40d653db96c9abab687c9afdfa0a7a234
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 14:15:12 +0530

    Create function_4.c

---
+++ function_4.c
@@ -0,0 +1,36 @@
+//Wriite a program for function
+//Date :  10/9/2022
+//Author : Yogita shidore
+
+
+#include<stdio.h>
+int main()
+{
+   //function declaration
+   void fun();
+   //function call
+   fun();
+   fun();
+   fun();
+   fun();
+   fun();
+   fun():
+   return(0);
+}
+void fun()
+{
+  //local variable declaration
+  int count = 0;
+  
+  //code
+  count = count + 1;
+  printf("\nvalue of num = %d", count);
+}
+
+
+//output = 1
+           1
+           1
+           1
+           1
+           1
