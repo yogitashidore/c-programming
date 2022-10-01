fa16b4143a0fb8f58e7203ae7b6c9af9ec27e752
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 16:35:41 +0530

    Create pointer_1.c

---
+++ pointer_1.c
@@ -0,0 +1,14 @@
+//write a program
+//Date : 22/09/2022
+//Author : Yogita shidoree
+
+#include<stdio.h>
+int main()
+{
+   //local variable declaration
+   int n = 10;
+   //code
+   printf("\n value of n = %d", n);
+   printf("\n address of n = %p", &n);
+   return(0);
+}
