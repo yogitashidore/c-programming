c83dbbf8483ff9086fb47721391eb13dfbb58b29
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 14:48:33 +0530

    Create greater_1.c

---
+++ greater_1.c
@@ -0,0 +1,12 @@
+//Wriite a program.
+//Date :  10/9/2022
+//Author : Yogita shidore
+
+#include<stdio.h>
+int main()
+{
+   int n1 = 30;
+   int n2 = 30;
+   n1>n2 ? printf("n1 is greater than n2"): n2>n1 ? printf("n2 is greater than n1"); printf("both are equal");
+   return(0);
+}
