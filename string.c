fc9c865f94a3e38e43dd4474d9d560f34406d5c1
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-01 16:06:37 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,17 @@
+//write a program
+//Date : 15/09/2022
+//Author : Yogita shidore
+
+#include<stdio.h>
+typedef char* string;
+typedef char* string;
+
+int main()
+{ 
+    string str = "Hello";
+    string str1 = "string two";
+    
+    printf("\n%s", str);
+    printf("\n%s", str1);
+    return(0);
+ }
