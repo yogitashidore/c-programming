d2a39e8d9d49c798547f534573c672027c33c81c
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-18 22:05:49 +0530

    Create file_handling.c

---
+++ file_handling.c
@@ -0,0 +1,16 @@
+#include<stduo.h>
+int main()
+{
+   //local variable declaration
+   FILE*fp = NULL;
+   fp = fopen("testfile.txt","a");
+  if(fp == NULL)
+  {
+     printf("\nenable to open the file, hence exiting..");
+     exit(1);
+     }
+      fprintf(fp,"\ntest data");
+      fclose(fp);
+      return(0);
+      }
+   
