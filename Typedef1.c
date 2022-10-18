0014fd2e2a82f842fcd6a616e95fe3de4c927073
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-18 08:55:14 +0530

    Create Typedef1.c

---
+++ Typedef1.c
@@ -0,0 +1,15 @@
+//print string using typedef
+#include<stdio.h>
+
+typedef char* string;
+typedef char* string;
+
+int main()
+{
+	string str = "YOGITA";
+	string str1 = "SHIDORE";
+
+	printf("\n%s", str);
+	printf("\n%s", str1);
+	return(0);
+}
