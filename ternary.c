d84eb7d2621677fdc50611483f68feae1afc610a
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-18 13:00:06 +0530

    Create ternary.c

---
+++ ternary.c
@@ -0,0 +1,13 @@
+//find maximum number useing ternary operator
+
+#include<stdio.h>
+ int main()
+{
+	//local variable declaration
+	int a = 10;
+	int b = 20;
+
+	//code
+	a > b ? printf("a is grater than b") : printf("b is grater than a");
+	return(0);
+}
