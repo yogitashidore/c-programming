ff186435a8924f3c6cdbf8e20732452f7611cdce
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-20 17:52:40 +0530

    Update and rename 1to9_no_forloop.c to for-loop2.c

---
+++ for-loop2.c
@@ -0,0 +1,38 @@
+//Athor : Yogita shidore
+//Date : 18/08/2022
+//write a c program to print 1to10 and last number is 10 using for loop.
+
+//input :
+
+//include std input output file
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+     int i;
+     for(i = 1; i < 10; i = i + 1)
+     {
+         printf("\nvalue of i=%d",i);
+     }
+         printf("\n\n\n last value of i=%d",i) ;
+        return(0); //returning value -successful
+}
+
+
+//output :  value of i=1
+            value of i=2
+            value of i=3
+            value of i=4
+            value of i=5
+            value of i=6
+            value of i=7
+            value of i=8
+            value of i=9
+            last value of i=10         
+              
+
+
+
+
+
