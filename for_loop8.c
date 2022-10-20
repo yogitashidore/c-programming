a6c8654ad313d38e24594a0dda78b96743a1a67e
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-20 18:31:16 +0530

    Update and rename Helloword_11times_forloop.c to for_loop8.c

--- for_loop8.c
+++ for_loop8.c
@@ -1,18 +1,18 @@
 //Author : Yogita shidore
 //Date : 18/08/2022
-//print Helloword program 11 times               
+//write a c program print Helloword program 11 times using for loop.               
        
 //input
 //include std input output file
-include<stdio.h>
+#include<stdio.h>
 
 int main()
 {
-   
+  
    int i;
    for (i = 0; i <= 10; i = i++)
    {
-      printf("Helloword program, this is my first code in c");
+      printf("Helloword program, this is my first code in c\n");
       
    }
    return(0); //returning value - successful
