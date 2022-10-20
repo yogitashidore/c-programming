9240bde1ed3915c6aa95816fe08ed83553c32946
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-20 17:37:38 +0530

    Update and rename -4to4_forloop.c to for_loop1.c

---
+++ for_loop1.c
@@ -0,0 +1,21 @@
+//Author : Yogita shidore
+//Date : 21/08/2022
+//write a c program print number -4 to 4 using for loop.           
+       
+//input
+//include std input output file
+include<stdio.h>
+
+int main()
+{
+    int i;
+    for (i = -4; i <= 4; i = i+1)
+    {
+    
+       printf("%d",i);
+    }
+    return(0); //returning value - successful
+}
+
+
+//output : -4 -3 -2 -1 0 1 2 3 4
