4b8fb9782df468e5e06bad463ef4bc7273a38ce0
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-25 14:08:39 +0530

    Update and rename 1to10_whileloop.c to w_loop2.c

--- w_loop2.c
+++ w_loop2.c
@@ -1,6 +1,6 @@
 //Athor : Yogita shidore
 //Date : 22/08/2022
-//print 1 to 10 number in while loop
+//write a c program print 1 to 10 number in while loop.
 
 //input :
 
@@ -10,7 +10,7 @@
 //entry point function
 int main()
 {
-  int i=1
+  int i=1;
   while(i<=10)
   {
     printf("\n%d",i);
@@ -19,3 +19,17 @@ int main()
 
    return(0); //returning value -successful
 }
+
+
+
+//output :
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+  10
