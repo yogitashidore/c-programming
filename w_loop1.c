7a0462f1c726678aeb2d168616def3c35c4c86e3
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-10-25 14:03:33 +0530

    Update and rename -4to4_whileloop.c to w_loop1.c

---
+++ w_loop1.c
@@ -0,0 +1,31 @@
+//Author : Yogita shidore
+//Date : 21/08/2022
+//write a c program print number -4 to 4 in while loop.          
+       
+//input
+//include std input output file
+#include<stdio.h>
+
+int main()
+{
+   int i = -4;
+   while(i <= 4)
+   {
+    
+     printf("%d\n",i);
+     i = i+1;
+ }
+   return(0); //returning value - successful
+}
+
+
+//output :  -4
+            -3
+            -2
+            -1
+             0
+             1
+             2
+             3
+             4
+                   
