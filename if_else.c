328fcd5b2fcce4d03238819b797f4d331a0b244d
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-02 12:17:22 +0530

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,21 @@
+//IFelse statement
+//Yogita shidore
+
+
+#include<stdio.h>
+int main()
+{
+	if (10)
+	{
+		printf("\nstatement is true");
+	}
+	else
+	{
+		printf("\nstatement is false");
+	}
+
+	return(0);
+}
+
+
+//output: statement is true
