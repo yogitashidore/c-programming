05de6df0b18a8657d82906427b09f8abe754a96a
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-02 09:06:57 +0530

    Create equal_or_not.c

---
+++ equal_or_not.c
@@ -0,0 +1,26 @@
+//write a c program find number are eual or not.
+//Yogita shidore
+//2/12/2022
+
+#include<stdio.h>
+int main()
+{
+	 //variable declaration
+	int num1, num2;
+	//code
+	printf("enter value of num1:");
+	scanf("%d", &num1);
+	printf("enter value of num2:");
+	scanf("%d", &num2);
+	//check both no are equal or not
+	if (num1 == num2)
+
+		printf("\n num1 and num2 are equal");
+	else
+		printf("\n num1 and num2 are not equal");
+
+	return(0);
+}
+
+
+	
