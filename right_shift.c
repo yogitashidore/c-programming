89275750bde9ab2862e1b80443f25c86f49e6040
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-02 11:19:51 +0530

    Create right_shift.c

---
+++ right_shift.c
@@ -0,0 +1,16 @@
+//right shift program
+//Yogita shidore
+//2/12/2022
+
+
+#include<stdio.h>
+int main()
+{
+	unsigned short int num1;
+	num1 = 1;
+	num1 = num1 << 10;
+	printf("\nvalue of num1 = %d", num1);
+	return(0);
+}
+
+//1024
