76b1f185b3f5cbe33943012f228f576dc673259a
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-02 09:13:17 +0530

    Create max_no.c

---
+++ max_no.c
@@ -0,0 +1,28 @@
+//find max no from two no.
+//Yogita shidore
+//2/12/2022
+
+#include<stdio.h>
+int main()
+{
+	 //variable declaration
+	int a, b;
+	//code
+	printf("enter value of a:");
+	scanf("%d", &a);
+	printf("enter value of b:");
+	scanf("%d", &b);
+	//check both no are equal or not
+	if (a > b)
+	{
+		printf("a>b");
+	}
+	else
+	{
+		printf("b>a");
+	}
+	return(0);
+}
+
+
+	
