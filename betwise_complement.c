7e9fc742a005fa71f9f98ca1c85b72918acb9307
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-02 12:07:56 +0530

    Create betwise_complement.c

---
+++ betwise_complement.c
@@ -0,0 +1,16 @@
+//bitwise complement
+//Yogita shidore
+//2/12/2022
+
+
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", ~30);
+	printf("%d\n", ~-12);
+	return(0);
+}
+
+
+//ouput: -31
+          11
