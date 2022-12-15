c695256a9acc7d1337c791baa402f82a62318c93
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-15 22:09:25 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+
+	printf("enter the number between 1-5:");
+	scanf("%d", &num)
+
+		switch (nums
+			{ S
+			case 1:
+				printf("\nAction for number 1");
+				break;
+
+			case 2:
+				printf("\nAction for number 1");
+				break;
+
+			case 3:
+				printf("\nAction for number 1");
+				break;
+
+			case 4:
+				printf("\nAction for number 1");
+				break;
+
+			case 5:
+				printf("\nAction for number 1");
+				break;
+
+			default:
+				printf("\ninvalid number");
+				break;
+
+
+			}
+	return(0);
+}
